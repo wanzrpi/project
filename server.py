3389b8abe94a10c9b1c5ea565d924b587e243b5b
JulieSanw <wenyinsan@aulacogen-149.dynamic2.rpi.edu>
2016-11-27 19:04:21 -0500

    merged

--- server.py
+++ server.py
@@ -12,15 +12,136 @@ DNS_3 = 'ec2-52-37-12-187.us-west-2.compute.amazonaws.com'
 PORT = 4400
 """
 
-#golobal variables
+#global variables
 log_loc = 'log.txt'
-file_sys = {}
-neighbor=[]
 server_addr = {}
 server_id = 0
-history=[]
-leader=0
+#leader_status: 1. 'waiting': waiting election for new leader  2. 'elected': the server knows who is the leader
+setting={'leader':0,'file_sys':{},'neighbor':[],'history':[],'applied':[], 'leader_status':'waiting'}
 
+#Wan
+def send_msg(m,server): ##function to write msg
+    ##todo
+    size = len(m)
+    server.wfile.write(str(size))
+    ret=server.request.recv(1024)
+    for i in range (0, int(math.floor(float(size) / 1024))):
+        server.wfile.write(msg[i*1024:(i+1)*1024])
+    if int(math.floor(float(size) / 1024))*1024 != size:
+        server.wfile.write(msg[int(math.floor(float(size) / 1024))*1024:size])
+def read_msg(server): ##function to read msg
+    size = int(server.request.recv(1024))
+    server.wfile.write(str(size))
+    msg = ""
+    for i in range (0, int(math.floor(float(size) / 1024))):
+        buff = server.request.recv(1024)
+        msg += buff
+    if int(math.floor(float(size) / 1024))*1024 != size:
+        buff = server.request.recv(size - int(math.floor(float(size) / 1024)*1024))
+        msg += buff
+    return msg
+def create(file_name):
+    if file_name in file_sys:
+        return "file {} already exists!".format(file_name)
+    else:
+        file_sys[file_name]=""
+        return "file {} created successfully!"
+
+
+def delete(file_name):
+    if file_name in file_sys:
+        del file_sys[file_name]
+        return "file {} deleted successfully!".format(file_name)
+    else:
+        return "file {} does not exist!".format(file_name)
+
+
+def read(file_name):
+    if file_name in file_sys:
+        return file_sys[file_name]
+    else:
+        return "file {} does not exist!".format(file_name)
+
+def append(file_name, msg):
+    if file_name in file_sys:
+        file_sys[file_name] += msg
+        return "message appended successfully!"
+    else:
+        return "file {} does not exist!".format(file_name)
+
+def process_command(command):
+    if command[0]=='a':
+        command = command.split(' ', 2)
+    else:
+        command = command.split(' ', 1)
+    if command[0] == 'create':
+        return create(command[1])
+    elif command[0] == 'delete':
+        return delete(command[1])
+    elif command[0] == 'append':
+        return append(command[1], command[2])
+    elif command[0] == 'read':
+        return read(command[1])
+
+        
+def add_history(lines):
+    msg=lines.split(' ', 2)
+    epoch=msg[0]
+    counter=msg[1]
+    operation=msg[2]
+    if epoch == len(setting['history']):
+        setting['history'].append([])
+        setting['history'][epoch].append(msg[2])
+            
+def get_history(log_loc):
+    f = open(log_loc, 'r')
+    for lines in f.readlines():
+        add_history(lines)
+    f.close()
+
+
+def get_history_max():
+    history_max=[]
+    for i in setting['history']:
+        history_max.append(len(i))
+    return history_max
+
+        
+def write_history():
+    f.open(log_loc, 'a')
+    for i in range (0,len(setting['history'])):
+        for j in range (0,len(setting['history'][i]])):
+            f.write('{} {} {}\n'.format(i, j, setting['history'][i][j]))
+
+def update(leader, history_max):
+    msg = json.dumps(history_max)
+    send_msg('history_match', leader)
+    rcv = read_msg(leader)
+    if rcv == 'history_match ACK':
+        send_msg(msg, leader)
+        new_max = json.loads(read_msg(leader))
+        new_history = json.loads(read_msg(leader))
+        return (new_max, new_history)
+    elif rcv == 'NOT LEADER':
+        return (None,None)
+    
+def recovery(leader):
+    history_max = get_history_max()
+    (new_max, new_history) = update(leader,history_max)
+    if new_max == None:
+        return False
+    for i in range (0,len(new_max)):
+        while len(setting['history'][i])>new_max[i]:
+            setting['history'][i].pop[len(setting['history'][i])-1]
+    for lines in json.loads(new_history):
+        add_history(lines)
+    write_history()
+    for lines in setting['history']:
+        return process_command(lines)
+    return True
+
+
+#San
 def election(server_id, epoch, counter, server_list):
     """
     Bully Election Algrorithm,
@@ -65,64 +186,19 @@ def broadcast(server_list, msg):
             sock.send(msg)
         except:
             pass
-def process_command(command):
-    if command[0]=='a':
-        command = command.split(' ', 2)
-    else:
-        command = command.split(' ', 1)
-    if command[0] == 'create':
-        file_sys[command[1]] = ""
-    elif command[0] == 'delete':
-        del file_sys[command[1]]
-    elif command[0] == 'append':
-        file_sys[command[1]] += command[2]
-
-        
-def add_history(lines):
-    msg=lines.split(' ', 2)
-    epoch=msg[0]
-    counter=msg[1]
-    operation=msg[2]
-    if epoch == len(history):
-        history.append([])
-        history[epoch].append(msg[2])
-            
-def get_history(log_loc):
-    f = open(log_loc, 'r')
-    for lines in f.readlines():
-        add_history(lines)
-    f.close()
-
-def write_history(log_loc,history):
-    f.open(log_loc, 'w')
-    for i in range (0,len(history)):
-        for j in range (0,len(history[i])):
-            f.write('{} {} {}\n'.format(i, j, history[i][j]))
-    
-def recovery(leader):
-    history_max = []
-    for i in history:
-        history_max.append(len(i))
-    (new_max, new_history) = update(leader,json.dumps(history_max))
-    new_max = json.loads(new_max)
-    for i in range (0,len(new_max)):
-        while len(history[i])>new_max[i]:
-            history[i].pop[len(history[i])-1]
-    for lines in json.loads(new_history):
-        add_history(lines)
-    write_history(log_loc, history)
-    for lines in history:
-        process_command(lines)
 
 def init(): ##init when process starts
+    ## process election to learn leader
     history=get_history(log_loc)
-    leader=election()##todo
-    recovery(leader)
-    
+    if recovery(leader):
+        ## start servering
+        return
+    else:
+        ##newelection
+        return
 
 ##server handler
 class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
-
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024)
@@ -145,42 +221,24 @@ class ThreadedTCPRequestHandler(SocketServer.BaseRequestHandler):
         elif ("ELECTED" in self.data):
             leader = self.data.split("\n")[1]
             self.leader = leader
+        elif("CREATE" in self.data):
 
-        elif("WRITE" in authenticated_requst):
-            #print(authenticated_requst)
-            write_file(self, authenticated_requst)
-            #if it is a primary copy then the server propagates the updated file
-            #to replicas, if a server can not be progagate too it is deamed failed and removed from
-            #the replica server list
-            if(self.leader == True):
-                file_name=authenticated_requst.split("\n")[1]
-                failed_servers=propagate_write_2(file_server.server_list,file_name, auth_port)
-                file_server.server_list.remove(failed_servers)
+        elif("DELET" in self.data):
+
+        elif("READ" in self.data):
 
 
-        elif("READ" in authenticated_requst):
-            if(file_server.primary_copy == True):
-                #send replica server info to client
-                self.request.send(file_server.server_list[file_server.last_replica])
-                #iterates to the next replica server, so that the following read requests are distributed fairly bewtween the replicas
-                if(len(file_server.server_list)==file_server.last_replica):
-                    file_server.last_replica=0
-                else:
-                    file_server.last_replica+=1
-            else:
-                #print(authenticated_requst)
-                read_file(self, authenticated_requst)
+        elif("WRITE" in self.data):
+        
 
-def parse_string_server_list(server_list):
-    server_list=[]
-    #host,port
-    server_strings=server_list.split("\n")
-    for servers in server_strings:
-        server_address=servers.split(",")
-        #adding the host and port (in int form) to the server list
-        server_list.append((server_address[0],int(server_address[1])))
-    return server_list
-##flo
+        elif("APPEND" in self.data):
+        
+
+        elif("EXIT" in self.data):
+
+        else:
+
+
 ##main    
 if __name__ == '__main__':
     server_id = sys.argv[1]
